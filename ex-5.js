9f292b16e0d322be331f2f2734dc54b89b58106c
KeeratiNon <keeratinon3@gmail.com>
2024-05-13 13:23:52 +0700

    answer-ex-1-7

--- ex-5.js
+++ ex-5.js
@@ -21,4 +21,7 @@ const employees = [
   },
 ];
 
-// Start coding here
+const updateEmployees = employees.pop();
+
+console.log(employees);
+
--- ex-7.js
+++ ex-7.js
@@ -42,7 +42,13 @@ let orders = [
 ];
 
 // Start coding here
-let isArray;
-let creditCardTypeOfBlindermann;
-let productQuantityOfJoannet;
-let totalPurchaseOfDary;
+let isArray = Array.isArray(orders);
+let creditCardTypeOfBlindermann = orders[2].creditCardType;
+orders[2].creditCardType = "visa";
+let productQuantityOfJoannet = orders[3].productQuantity;
+let totalPurchaseOfDary = orders[1].productPrice * orders[1].productQuantity;
+
+// console.log(isArray, creditCardTypeOfBlindermann, productQuantityOfJoannet, totalPurchaseOfDary);
+
+orders.shift();
+console.log(orders);
